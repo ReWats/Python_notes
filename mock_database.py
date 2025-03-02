ecb412b8979343c910055f770dbcf6840fcadca8
Rebecca Watson <181154487+ReWats@users.noreply.github.com>
2025-03-02 08:14:20 +0000

    Create mock_database.py

---
+++ mock_database.py
@@ -0,0 +1,34 @@
+characters = [
+    {
+        "id": 2,
+        "first_name": "Hermione",
+        "last_name": "Granger",
+        "house": "Gryffindor",
+        "wand": "Vine wood, Dragon heartstring core",
+        "IQ": 150
+    },
+    {
+        "id": 3,
+        "first_name": "Ron",
+        "last_name": "Weasley",
+        "house": "Gryffindor",
+        "wand": "Willow, Unicorn hair core",
+        "IQ": 100
+    },
+    {
+        "id": 1,
+        "first_name": "Harry",
+        "last_name": "Potter",
+        "house": "Gryffindor",
+        "wand": "Holly, Phoenix feather core",
+        "IQ": 110
+    },
+    {
+        "id": 4,
+        "first_name": "Draco",
+        "last_name": "Malfoy",
+        "house": "Slytherin",
+        "wand": "Hawthorn, Unicorn hair core",
+        "IQ": 100
+    }
+]
